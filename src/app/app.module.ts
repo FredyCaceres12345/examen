cb7c3f2ffab3d407828e46e05d74d364a9fcdc1e
FredyCaceres12345 <fredylorenzocaceres95@gmail.com>
2024-05-31 19:20:09 -0400

    mi segundo examen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,28 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { initializeAppCheck, ReCaptchaEnterpriseProvider, provideAppCheck } from '@angular/fire/app-check';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
+import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai-preview';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+     provideFirebaseApp(() => initializeApp({"projectId":"lenguajelp5",
+     "appId":"1:493821626701:web:d16f4a0d92c37cda118f04","storageBucket":"lenguajelp5.appspot.com",
+     "apiKey":"AIzaSyCGQao-z1hqRharaFeO6eDbN6forbFSVK4","authDomain":"lenguajelp5.firebaseapp.com",
+     "messagingSenderId":"493821626701"})), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()),
+      ScreenTrackingService, UserTrackingService,  provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideFunctions(() => getFunctions()), provideMessaging(() => getMessaging()), providePerformance(() => getPerformance()), provideStorage(() => getStorage()), provideRemoteConfig(() => getRemoteConfig()), provideVertexAI(() => getVertexAI())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
